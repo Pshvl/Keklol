8db2316b6ddf56b8c5f63110e118468560b202ea
prettyJaba <114904092+prettyJaba@users.noreply.github.com>
2023-10-10 23:06:35 +0400

    Класс ArrayTabulatedFunction покрыт тестами

--- LR2/src/test/java/functions/ArrayTabulatedFunctionTest.java
+++ LR2/src/test/java/functions/ArrayTabulatedFunctionTest.java
@@ -1,61 +1,117 @@
 package functions;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayTabulatedFunctionTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    double [] xValuesTest = {3.3, 4.3, 5.9, 9, 11.32};
+    double [] yValuesTest = {36, 2.2, 0.1, 42, 6.54};
+    ArrayTabulatedFunction testArray = new ArrayTabulatedFunction(xValuesTest, yValuesTest);
 
     @Test
     void getCount() {
+        int actual = testArray.getCount();
+        int expected = 5;
+        assertEquals(expected, actual);
     }
 
     @Test
     void getX() {
-    }
-
-    @Test
-    void getY() {
+        double actual = testArray.getX(2);
+        double expected = 5.9;
+        assertEquals(expected, actual);
     }
 
     @Test
     void setY() {
+        testArray.setY(3,4.13);
+        double actual = testArray.getY(3);
+        double expected = 4.13;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getY() {
+        double actual = testArray.getY(3);
+        double expected = 42;
+        assertEquals(expected, actual);
     }
 
     @Test
     void leftBound() {
+        double actual = testArray.leftBound();
+        double expected = 3.3;
+        assertEquals(expected, actual);
     }
 
     @Test
     void rightBound() {
+        double actual = testArray.rightBound();
+        double expected = 11.32;
+        assertEquals(expected, actual);
     }
 
     @Test
     void indexOfX() {
+        double actual = testArray.indexOfX(5.9);
+        double expected = 2;
+        assertEquals(expected, actual);
+
+        actual = testArray.indexOfX(1122);
+        expected = -1;
+        assertEquals(expected, actual);
     }
 
     @Test
     void indexOfY() {
+        double actual = testArray.indexOfY(6.54);
+        double expected = 4;
+        assertEquals(expected, actual);
+
+        actual = testArray.indexOfY(1122);
+        expected = -1;
+        assertEquals(expected, actual);
     }
 
     @Test
     void floorIndexOfX() {
+        double actual = testArray.floorIndexOfX(2);
+        double expected = 0;
+        assertEquals(expected, actual);
+
+        actual = testArray.floorIndexOfX(1122);
+        expected = 5;
+        assertEquals(expected, actual);
+
+        actual = testArray.floorIndexOfX(8);
+        expected = 3;
+        assertEquals(expected, actual);
     }
 
     @Test
     void extrapolateLeft() {
+        double actual = testArray.extrapolateLeft(1.5);
+        double expected = 96.84;
+        assertEquals(expected, actual, 0.000001);
     }
 
     @Test
     void extrapolateRight() {
+        double actual = testArray.extrapolateRight(12.8);
+        double expected = 42 - 134.748/2.32;
+        assertEquals(expected, actual, 0.000001);
     }
 
     @Test
     void interpolate() {
+        double leftX = testArray.getX(testArray.floorIndexOfX(6)-1);
+        double rightX = testArray.getX(testArray.floorIndexOfX(6));
+        double leftY = testArray.getY(testArray.floorIndexOfX(6)-1);
+        double rightY = testArray.getY(testArray.floorIndexOfX(6));
+        double actual = testArray.interpolate(6, leftX, rightX, leftY, rightY);
+        double expected = 0.1 + 4.19/3.1;
+        assertEquals(expected, actual, 0.000001);
     }
 }
