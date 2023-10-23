914843de5af63b670ce88859b44630275bccc532
prettyJaba <114904092+prettyJaba@users.noreply.github.com>
2023-10-24 03:34:32 +0400

    Добавлены методы в IdentityFunction и покрыты тестами

--- LR2/src/main/java/functions/IdentityFunction.java
+++ LR2/src/main/java/functions/IdentityFunction.java
@@ -5,4 +5,26 @@ public class IdentityFunction implements MathFunction
     public double apply(double x) {
         return x;
     }
+
+    public final String toString() {
+        return "IdentityFunction{}";
+    }
+
+    public boolean equals(Object obj) {
+        return this.getClass() == obj.getClass();
+    }
+
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public Object clone() {
+           /* try {
+                return super.clone();
+            } catch (CloneNotSupportedException e) {
+                System.out.println(e.getMessage());
+                return null;
+            } */
+        return new IdentityFunction();
+    }
 }
--- LR2/src/test/java/functions/IdentityFunctionTest.java
+++ LR2/src/test/java/functions/IdentityFunctionTest.java
@@ -13,4 +13,33 @@ public class IdentityFunctionTest {
         assertEquals(Q.apply(-4), -4);
         assertEquals(Q.apply(0.8975), 0.8975);
     }
+
+    @Test
+    public void testToString() {
+        IdentityFunction func = new IdentityFunction();
+        assertEquals(func.toString(), "IdentityFunction{}");
+    }
+
+    @Test
+    public void testEquals() {
+        IdentityFunction idenFunc_1 = new IdentityFunction();
+        IdentityFunction idenFunc_2 = new IdentityFunction();
+        ZeroFunction zeroFunc = new ZeroFunction();
+        assertTrue(idenFunc_1.equals(idenFunc_2));
+        assertFalse(idenFunc_1.equals(zeroFunc));
+    }
+
+    @Test
+    public void testHashCode() {
+        IdentityFunction idenFunc_1 = new IdentityFunction();
+        IdentityFunction idenFunc_2 = new IdentityFunction();
+        assertNotEquals(idenFunc_1.hashCode(), idenFunc_2.hashCode());
+    }
+
+    @Test
+    public void testClone() throws CloneNotSupportedException {
+        IdentityFunction idenFunc_1 = new IdentityFunction();
+        IdentityFunction idenFunc_2 = (IdentityFunction) idenFunc_1.clone();
+        assertEquals(idenFunc_1, idenFunc_2);
+    }
 }
