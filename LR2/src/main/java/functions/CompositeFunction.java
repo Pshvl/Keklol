d7a032c6f1b65ea63a63b1090d11e56bf306cc06
Pshvl <145437847+Pshvl@users.noreply.github.com>
2023-11-07 23:11:46 +0400

    пофикс

--- LR2/src/main/java/functions/CompositeFunction.java
+++ LR2/src/main/java/functions/CompositeFunction.java
@@ -1,15 +1,14 @@
 package functions;//1
 
 class CompositeFunction implements MathFunction{
-private final MathFunction firstFunction;
-private final MathFunction secondFunction;
+    private final MathFunction firstFunction;
+    private final MathFunction secondFunction;
 
-public CompositeFunction(MathFunction firstFunction,MathFunction secondFunction){
+    public CompositeFunction(MathFunction firstFunction,MathFunction secondFunction){
         this.firstFunction=firstFunction;
         this.secondFunction=secondFunction;
-        }
-public double apply(double x){
+    }
+    public double apply(double x){
         return secondFunction.apply(firstFunction.apply(x));
-        }
-        }
-
+    }
+}
