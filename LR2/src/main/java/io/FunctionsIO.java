de26e773f9793e1cd0165a9663df151d94b20506
prettyJaba <114904092+prettyJaba@users.noreply.github.com>
2023-11-21 00:54:18 +0400

    Создан класс FunctionsIo

---
+++ LR2/src/main/java/io/FunctionsIO.java
@@ -0,0 +1,15 @@
+package io;
+
+import functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+
+public final class FunctionsIO {
+    private FunctionsIO() {
+        throw new UnsupportedOperationException("Functions IO не может иметь наследников и экземпляров");
+    }
+
+    static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+
+    }
+}
