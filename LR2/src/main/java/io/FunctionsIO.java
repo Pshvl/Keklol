eaf4876dce3ccc43723310124ece9421494a5689
prettyJaba <114904092+prettyJaba@users.noreply.github.com>
2023-11-21 01:34:08 +0400

    Создан метод writeTabulatedFunction и класс TabulatedFunctionFileWriter

--- LR2/src/main/java/io/FunctionsIO.java
+++ LR2/src/main/java/io/FunctionsIO.java
@@ -1,8 +1,10 @@
 package io;
 
+import functions.Point;
 import functions.TabulatedFunction;
 
 import java.io.BufferedWriter;
+import java.io.PrintWriter;
 
 public final class FunctionsIO {
     private FunctionsIO() {
@@ -10,6 +12,11 @@ public final class FunctionsIO {
     }
 
     static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
-
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+        printWriter.flush();
     }
 }
---
+++ LR2/src/main/java/io/TabulatedFunctionFileWriter.java
@@ -0,0 +1,31 @@
+package io;
+
+import functions.ArrayTabulatedFunction;
+import functions.LinkedListTabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static io.FunctionsIO.writeTabulatedFunction;
+
+public class TabulatedFunctionFileWriter {
+    public static void main(String[] args) {
+        try(BufferedWriter arrayWriter = new BufferedWriter(new FileWriter("output/array function.txt"));
+            BufferedWriter linkedListWriter = new BufferedWriter(new FileWriter("output/linked list function.txt"))){
+
+            double [] xValuesTest = {3.3, 4.3, 5.9, 9, 11.32};
+            double [] yValuesTest = {36, 2.2, 0.1, 42, 6.54};
+
+            ArrayTabulatedFunction arrayFunc = new ArrayTabulatedFunction(xValuesTest, yValuesTest);
+            LinkedListTabulatedFunction linkListFunc = new LinkedListTabulatedFunction(xValuesTest, yValuesTest);
+
+            writeTabulatedFunction(arrayWriter, arrayFunc);
+            writeTabulatedFunction(linkedListWriter, linkListFunc);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
