067dbe2f55e32f58b02410bb0bc1eb38b69c1cdd
prettyJaba <114904092+prettyJaba@users.noreply.github.com>
2023-12-03 22:12:21 +0400

    Создан класс MultiplyingTaskExecutor

---
+++ LR2/src/main/java/concurrent/MultiplyingTaskExecutor.java
@@ -0,0 +1,26 @@
+package concurrent;
+
+import functions.LinkedListTabulatedFunction;
+import functions.TabulatedFunction;
+import functions.UnitFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplyingTaskExecutor {
+    public static void main(String[] args) throws InterruptedException {
+        TabulatedFunction linkListFunc = new LinkedListTabulatedFunction(new UnitFunction(), 1, 1000, 10);
+        List<Thread> threadList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            Runnable task = new MultiplyingTask(linkListFunc);
+            threadList.add(new Thread(task));
+        }
+
+        for (Thread t : threadList) {
+            t.start();
+        }
+        Thread.sleep(200);
+        System.out.println(linkListFunc);
+    }
+}
